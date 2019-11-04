752e45b9dfc1635ff84ba04337335fc0ab0c4372
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-11-05 03:20:43 +0900

    hello world marquee

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate ncurses;
 
+use std::cmp::{min, max};
 use std::{thread, time};
 use ncurses::*;
 
@@ -10,11 +11,33 @@ fn get_terminal_size(win: WINDOW) -> (i32, i32) {
     (width, height)
 }
 
-fn marquee(text: &str) {
+fn marquee_line(width: i32, line: &str, count: i32) {
+    let line_length = line.len() as i32;
+    let count = count % (width + line_length);
+    let leftermost_offset = max(0, width - count);
+    let visible_length =
+        if leftermost_offset == 0 {
+            line_length - (count - width)
+        } else {
+            min(line_length, width - leftermost_offset)
+        } as usize;
+    let sliced_line =
+        if leftermost_offset == 0 {
+            let slice_len = (count - width) as usize;
+            &line[slice_len..]
+        } else {
+            &line[..visible_length]
+        };
+    let space = " ".repeat(leftermost_offset as usize);
+    let showtext = format!("{}{}", space, sliced_line);
+    addstr(showtext.as_str());
+}
+
+fn marquee(win: WINDOW, text: &str) {
+    let (width, height) = get_terminal_size(win);
     let mut counter = 0;
     loop {
-        let showtext = format!("{} {}", text, counter);
-        addstr(showtext.as_str());
+        marquee_line(width, text, counter);
         refresh();
         thread::sleep(time::Duration::from_millis(100));
         clear();
@@ -25,6 +48,7 @@ fn marquee(text: &str) {
 
 fn main() {
     let window = initscr();
-    marquee("Hello, world!");
+    curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
+    marquee(window, "Hello, world!");
     endwin();
 }
