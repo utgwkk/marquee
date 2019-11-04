865c818ef320ea7c98bcfa3d13cafe7b05f598e5
UTAGAWA Kiki <utagawakiki@gmail.com>
2019-11-05 02:34:09 +0900

    show terminal size

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,17 @@ extern crate ncurses;
 
 use ncurses::*;
 
+fn get_terminal_size(win: WINDOW) -> (i32, i32) {
+    let mut width = 0;
+    let mut height = 0;
+    getmaxyx(win, &mut height, &mut width);
+    (width, height)
+}
+
 fn main() {
-    initscr();
-    addstr("Hello, world!");
+    let window = initscr();
+    let (width, height) = get_terminal_size(window);
+    addstr(format!("width = {}, height = {}", width, height).as_str());
     refresh();
     getch();
     endwin();
